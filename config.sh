1b4907f76c5978d01a4e08ebfd6282a460c4733b
Julian Hector <julian.r.hector@gmail.com>
2015-05-29 14:49:41 -0700

    Modify config.sh to use my forked repos

--- config.sh
+++ config.sh
@@ -30,8 +30,8 @@ case `uname` in
 	exit -1
 esac
 
-GITREPO=${GITREPO:-"git://github.com/mozilla-b2g/b2g-manifest"}
-BRANCH=${BRANCH:-master}
+GITREPO=${GITREPO:-"git://github.com/jhector/b2g-manifest"}
+BRANCH=${BRANCH:-b2g-nexus-5-l-sepolicy}
 
 while [ $# -ge 1 ]; do
 	case $1 in
